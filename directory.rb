e7d31786f4845449d8ff305071399606065854b4
habakkukyong <habakkukyong@gmail.com>
2018-12-18 21:42:41 +0000

    Implement Student as a class, prompt input of cohort with default value in case of blank input, change symbol keys and values to strings

--- directory.rb
+++ directory.rb
@@ -1,3 +1,64 @@
+#=begin
+class Student
+  def initialize(name, id, cohort="November", hobbies=["Source-closing", "patent-trolling", "predatory litigation"], country="Nearest Seastead")
+    @name = name
+    @id = id
+    @cohort = cohort
+    @hobbies = hobbies.length > 1 ? hobbies[0..-2].join(", ") + " and " + hobbies.last : hobbies.first
+    @country = country
+  end
+  def set_cohort(cohort)
+    @cohort = cohort
+  end
+  def print_deets
+    puts "#{@id}. Name:    #{@name}\n   Cohort:  #{@cohort}\n   Hobbies: #{@hobbies}\n   Country: #{@country}"
+  end
+end
+
+def print_header
+  puts "The students of Villains Academy".center(80)
+  puts "-------------".center(80)
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  students_num = 1
+  while !name.empty? do
+    student = Student.new(name, students_num)
+    puts "Cohort?"
+    cohort = gets.chomp
+    student.set_cohort(cohort) if !cohort.empty?
+    students << student
+    puts "Now we have #{students_num} students"
+    name = gets.chomp
+    students_num += 1
+  end
+  students
+end
+
+def print(students)
+  count_var = 0
+  while count_var < students.length
+    index = count_var
+    students[count_var].print_deets
+    count_var += 1
+  end
+end
+
+def print_footer(names)
+  puts "Overall we have #{names.count} great students".center(80)
+end
+
+students =input_students
+print_header
+print(students)
+print_footer(students)
+#=end
+
+=begin
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -29,3 +90,4 @@ end
 print_header
 print(students)
 print_footer(students)
+=end
